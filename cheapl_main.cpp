90b78e5c52190babd3816cae7a447ea531e00841
Danny <dh.programming@havenith.net>
2013-12-10 00:07:42 +0100

    WIP linking with asound now

--- cheapl_main.cpp
+++ cheapl_main.cpp
@@ -5,20 +5,58 @@
  *      Author: danny
  */
 #include <alsa/asoundlib.h>
+#include <iostream>
 
 template< int (*next_func)(int*)>
 class alsa_iterator
 {
 public:
+	typedef int value_type;
 	alsa_iterator(): index(-1){}
-	alsa_iterator &operator++() { next_func( index);  return *this;}
+	alsa_iterator &operator++() { next_func( &index);  return *this;}
+	int operator*() const
+	{
+		return index;
+	}
+
+	bool operator<( const alsa_iterator &other) const
+	{
+		return index < other.index;
+	}
+
+	bool operator!=( const alsa_iterator &other) const
+	{
+		return index != other.index;
+	}
 private:
 	int index;
 };
 
+template< int (*next_func)(int*)>
+class alsa_container
+{
+public:
+	typedef alsa_iterator<next_func> iterator_type;
+	typedef typename iterator_type::value_type value_type;
+	iterator_type begin()
+	{
+		return ++iterator_type();
+	}
+	iterator_type end()
+	{
+		return iterator_type();
+	}
+};
+
+using snd_cards = alsa_container< &snd_card_next>;
+
 int main()
 {
 	alsa_iterator<&snd_card_next> snd_card_iterator;
+	for (int card: snd_cards())
+	{
+		std::cout << card << '\n';
+	}
 	return 0;
 }
 
